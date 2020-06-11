0536f4e51e8a00c0ffbb160f00d046ab952979cd
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-11 13:54:49 -0300

    ident and coment list module

--- src/commands/list.js
+++ src/commands/list.js
@@ -26,45 +26,97 @@ if (require('../credencials.json')) {
   throw new Error('credencials file not found! [credencials.json || credencials.template.json]');
 }
 
+// COMMAND TEMPLATE
+// callback function params (client, channel, user, message)
+/*
+/ {
+/   cmd: '<command-name>',
+/   description: '<command-description>',
+/
+/   callback function
+/   func: (client, channel) => commandFunction(
+/     client, <default-param>
+/     channel <default-param>
+/   )
+/ }
+*/
+
+// commands export
 module.exports = [
   {
     cmd: 'hello',
     description: 'Dizer hello!',
-    func: (client, channel, user) => hello(client, channel, user)
+    func: (client, channel, user) => hello(
+      client,
+      channel,
+      user
+    )
   },
   {
     cmd: 'color',
     description: 'Mudar a cor do bot.',
-    func: (client, channel, user, message) => color(client, channel, user, message)
+    func: (client, channel, user, message) => color(
+      client,
+      channel,
+      user,
+      message
+    )
   },
   {
     cmd: 'colorlist',
     description: 'Ver a lista de cores do bot.',
-    func: (client, channel) => colorList(client, channel)
+    func: (client, channel) => colorList(
+      client,
+      channel
+    )
   },
   {
     cmd: 'dice',
     description: 'Girar um dado de seis lados.',
-    func: (client, channel, user) => dice(client, channel, user)
+    func: (client, channel, user) => dice(
+      client,
+      channel,
+      user
+    )
   },
   {
     cmd: 'ban',
     description: 'Banir um úsuario do chat!',
-    func: (client, channel, user, message) => ban(client, channel, user, message, streamer)
+    func: (client, channel, user, message) => ban(
+      client,
+      channel,
+      user,
+      message,
+      streamer
+    )
   },
   {
     cmd: 'clear',
     description: 'Limpar as mensagens do chat!',
-    func: (client, channel, user) => clear(client, channel, user, streamer)
+    func: (client, channel, user) => clear(
+      client,
+      channel,
+      user,
+      streamer
+    )
   },
   {
     cmd: 'social',
     description: 'Redes sociais do streamer.',
-    func: (client, channel) => social(client, channel, socials)
+    func: (client, channel) => social(
+      client,
+      channel,
+      socials
+    )
   },
   {
     cmd: 'git',
     description: 'GitHub do streamer.',
-    func: (client, channel) => git(client, channel, streamer, gitLink)
+    func: (client, channel) => git(
+      client,
+      channel,
+      streamer,
+      gitLink
+    )
   }
 ]
