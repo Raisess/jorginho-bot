c5a7e07ef6081f8c145b85efa79137d6e85ea422
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-11 14:55:35 -0300

    updated clear command

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,7 +1,15 @@
 module.exports = (client, channel, user, streamer) => {
   if (user.mod || user.username.toLowerCase() === streamer) {
-    return client.clear(channel);
+    client.clear(channel);
+
+    return client.say(
+      channel,
+      `@${user.username} me pediu para limpar o chat!
+    `);
   }
 
-  return client.say(channel, `@${user.username} você não é mod meu chapa...`);
+  return client.say(
+    channel,
+    `@${user.username} você não é mod meu chapa...`
+  );
 };
