891e87f9533c2234151ff80917704f83bba5b09e
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-23 17:01:38 -0300

    updated social cmd and credencials template

--- src/commands/social.js
+++ src/commands/social.js
@@ -1,4 +1,4 @@
 module.exports = (client, channel, socials) => client.say(
   channel,
-  `Siga-me nas redes sociais twitter: https://twitter.com/${socials[0]} | instagram: https://instagram.com/${socials[1]}`
+  `Siga-me nas redes sociais twitter: https://twitter.com/danill1_ | instagram: https://instagram.com/danillu1_ | instagram do streamer: https://instagram.com/${socials[0]}`
 );
