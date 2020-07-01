dcd1c3d02462290164dcb950f23c06a179ee81ff
danillu <48914278+Raisess@users.noreply.github.com>
2020-07-01 15:36:44 -0300

    fixed followage command

--- src/commands/followage.js
+++ src/commands/followage.js
@@ -3,6 +3,6 @@ const fetch = require('node-fetch');
 module.exports = (client, channel, user, streamer) => {
   return fetch(`https://api.2g.be/twitch/followage/${streamer}/${user.username}?format=mwdhms`)
     .then(res => res.text())
-    .then(text => client.say(channel, `@${user.username} está seguindo a ${text}`))
+    .then(text => client.say(channel, text))
     .catch(() => client.say(channel, 'algo de errado não está certo, não consigo ver a quanto tempo vc segue...'));
 }
